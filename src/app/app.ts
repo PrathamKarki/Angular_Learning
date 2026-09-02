3d0d5ba7a5b5c96f0fe932eebedfa79b8a19c1d4
Pratham Karki <you@example.com>
2026-09-02 21:46:49 +0545

    Data binding in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('learn_angular');
+  protected readonly title = signal('HRM');
+
+  header: string = "App Component";
+
+  a: number = 5.1;
+  b: number = 7;
+
+  secondTitle: string = "The user is active";
+  secondTitl2 = signal("The user is active");
+
+  isChecked: boolean = true;
+  isChecked2 = signal<boolean>(true);
+
+  dothis: string = this.isChecked ? "True" : "false"; // ischecked
+
+  changeCheckbox(value: boolean): string {
+    this.isChecked2.set(false);
+
+    this.isChecked = !this.isChecked;
+    this.isChecked = value;
+    this.dothis = this.isChecked ? "True" : "false"
+    return "";
+  }
 }

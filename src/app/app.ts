cf941701fecd4c7b6da49fdd3dbcd366b5420e02
Pratham Karki <you@example.com>
2026-09-06 10:11:04 +0545

    ng_directives in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,43 +1,18 @@
 import { Component, ElementRef, signal, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { Home } from '../home/home'
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [FormsModule, Home],
+  imports: [FormsModule, CommonModule],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
 export class App {
-  @ViewChild('TestCheckboxTemp') tempCheckbox: ElementRef<HTMLInputElement> | undefined;
-  protected readonly title = signal('HRM');
 
-  header: string = "App Component";
+  cities: string[] = ['NYC', 'LA', 'California', 'Ktm'];
+  showField: boolean = true;
+  staticField: string = "Yes";
 
-  a: number = 5.1;
-  b: number = 7;
 
-  secondTitle: string = "The user is active";
-  secondTitl2 = signal("The user is active");
-
-  isChecked: boolean = true;
-  isChecked2 = signal<boolean>(true);
-
-  dothis: string = this.isChecked ? "True" : "false"; // ischecked
-
-  changeCheckbox() {
-    // this.isChecked2.set(false);
-
-    // this.isChecked = !this.isChecked;
-    // // this.isChecked = value;
-    // this.dothis = this.isChecked ? "True" : "false"
-    // return "";
-    // this.isChecked = <boolean>this.tempCheckbox?.nativeElement.checked;
-    console.log("Reached");
-  }
-
-  handleChildEvent(value: string) {
-    this.header = value;
-  }
 }
